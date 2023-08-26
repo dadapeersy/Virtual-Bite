4550773bf7f8ecb61fd740cf2734c82d4c74c80c
Sunil Aadewar <sunilaadewar@gmail.com>
2023-08-26 17:59:49 +0530

    Timestamp added for users

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
+    username: {type: String, require: true, unique: true},
+    password: {type: String, require: true},
+    savedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "recipes"}],
+    timeStamp: {type: Date, default: Date.now},
 });
 
 export const UserModel = mongoose.model("users", UserSchema);
